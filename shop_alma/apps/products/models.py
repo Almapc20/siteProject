be6ffa7f5ec406acfdccaf87eb1d1fc533a8f50c
somaye <91431240+savadkoohy@users.noreply.github.com>
2025-02-07 20:02:38 +0330

    change product models

--- shop_alma/apps/products/models.py
+++ shop_alma/apps/products/models.py
@@ -1,77 +1,79 @@
 from django.db import models
-from django.urls import reverse
+from email.mime import image
 from utils import FileUpload
 from django.utils import timezone
-from ckeditor_uploader.fields import RichTextUploadingField
-from ckeditor.fields import RichTextField
+from django.urls import reverse
+from django_ckeditor_5.fields import CKEditor5Field
+# from ckeditor_uploader.fields import RichTextUploadingField
+# from ckeditor.fields import RichTextField
 
 
-# -------------------------------------------------------------------------------------
+
 class Brand(models.Model):
-    brand_title= models.CharField(max_length=100, verbose_name='نام برند')
-    file_upload= FileUpload('image', 'brand')
-    image_name= models.ImageField(upload_to=file_upload.upload_to, verbose_name='تصویر برند')
+    brand_title= models.CharField(max_length=100, verbose_name="نام برند")
+    file_uplaod= FileUpload('images', 'brand')
     slug= models.SlugField(max_length=200, null=True)
+    image_name= models.ImageField(upload_to=file_uplaod.upload_to, verbose_name="تصویر گروه کالا")
     
-    def __str__(self):
+    def __str__(self) -> str:
         return self.brand_title
     
     class Meta:
-        verbose_name= 'برند'
-        verbose_name_plural= 'برند ها'
+        verbose_name= "برند"
+        verbose_name_plural= "برند ها"
 
-# #---------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------
 class ProductGroup(models.Model):
-    group_title= models.CharField(max_length=100, verbose_name='عنوان گروه کالا')
-    file_upload= FileUpload('image', 'product_group')
-    image_name= models.ImageField(upload_to=file_upload.upload_to, verbose_name='تصویر کروه کالا')
-    # description= RichTextField(blank= True, null= True, verbose_name='توضیحات گروه کالا')
-    description= RichTextUploadingField(config_name= 'special', blank= True)    
-    is_active= models.BooleanField(default= True, blank= True, verbose_name='وضعیت فعال / غیر فعال')
-    register_date= models.DateTimeField(auto_now_add=True, verbose_name='تاریخ درج')
-    published_date=models.DateTimeField(default= timezone.now, verbose_name='تاریخ انتشار')
-    update_date= models.DateTimeField(auto_now= True, verbose_name='تاریخ به روز رسانی')
-    group_parent= models.ForeignKey('ProductGroup', on_delete=models.CASCADE, verbose_name='والد گروه کالا', blank= True, null= True, related_name= 'groups')
+    group_title= models.CharField(max_length=100, verbose_name="عنوان گروه کالا")
+    file_uplaod= FileUpload('images', 'product_group')
+    image_name= models.ImageField(upload_to=file_uplaod.upload_to, verbose_name="تصویر گروه کالا")
+    description= models.TextField(blank=True, verbose_name="توضیحات گروه کالا", null=True)
+    is_active= models.BooleanField(default=True,blank=True, verbose_name="وضعیت فعال / غیر فعال")
+    group_parent= models.ForeignKey('ProductGroup', on_delete=models.CASCADE, verbose_name="والد گروه کالا", blank=True, null=True, related_name='groups')
     slug= models.SlugField(max_length=200, null=True)
+    register_date= models.DateTimeField(auto_now_add=True, verbose_name="تاریخ درج", )
+    published_date= models.DateTimeField(default=timezone.now, verbose_name="تاریخ انتشار")
+    update_date= models.DateTimeField(auto_now=True, verbose_name="تاریخ آخرین بروزرسانی")
     
     
-    def __str__(self):
+    def __str__(self) -> str:
         return self.group_title
     
     class Meta:
-        verbose_name= 'گروه ها'
-        verbose_name_plural= 'گروه های کالا'
+        verbose_name= "گروه کالا"
+        verbose_name_plural= "گروه های کالا"
     
-# #---------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------
 class Feature(models.Model):
-    feature_name= models.CharField(max_length=100, verbose_name='عنوان ویژگی')
-    product_group= models.ManyToManyField(ProductGroup, verbose_name='گروه غذاها', related_name='features_of_groups')
+    feature_name= models.CharField(max_length=100, verbose_name="نام ویژگی")    
+    product_group= models.ManyToManyField(ProductGroup, verbose_name="گروه کالا",related_name="feature_of_groups")
     
-    def __str__(self):
+    def __str__(self) -> str:
         return self.feature_name
     
     class Meta:
-        verbose_name= 'ویژگی'
-        verbose_name_plural= 'ویژگی ها'
+        verbose_name= "ویژگی کالا"
+        verbose_name_plural= "ویژگی های کالا"
     
-    
-# #---------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------
 class Product(models.Model):
-    product_name= models.CharField(max_length=500, verbose_name='نام کالا')
-    summery_description= models.TextField(default="", blank= True, null= True, verbose_name= ' مختصری از توضیحات کالا')
-    description= RichTextUploadingField(config_name= 'special', blank= True)    
-    file_upload= FileUpload('image', 'product')
-    image_name= models.ImageField(upload_to=file_upload.upload_to, verbose_name='تصویر کالا')
-    price= models.PositiveIntegerField(default=0, verbose_name='قیمت کالا')
-    is_active= models.BooleanField(default= True, blank= True, verbose_name='وضعیت فعال / غیر فعال')
-    register_date= models.DateTimeField(auto_now_add=True, verbose_name='تاریخ درج')
-    published_date=models.DateTimeField(default= timezone.now, verbose_name='تاریخ انتشار')
-    update_date= models.DateTimeField(auto_now= True, verbose_name='تاریخ به روز رسانی')
-    product_group= models.ManyToManyField(ProductGroup, verbose_name= 'گروه کالاها', related_name= 'products_of_groups')
-    brand= models.ForeignKey(Brand, verbose_name='برند کالا', on_delete=models.CASCADE, null= True, related_name='brands')
+    product_name= models.CharField(max_length=500, verbose_name="نام کالا")
+    summery_description= models.TextField(default="", blank=True, null=True, verbose_name="توضیحات کالا")    
+    # description= RichTextUploadingField(config_name= 'special', blank= True)    
+    description=CKEditor5Field('Text', config_name='extends', blank= True)    
+    file_uplaod= FileUpload('images', 'product')
+    image_name= models.ImageField(upload_to=file_uplaod.upload_to, verbose_name="تصویر کالا")
+    price= models.PositiveIntegerField(default=0, verbose_name="قیمت کالا")
+    product_group= models.ManyToManyField(ProductGroup,verbose_name="گروه کالا", related_name="product_of_groups")
+    features= models.ManyToManyField(Feature, through="ProductFeature")
+    brand= models.ForeignKey(Brand, verbose_name="برنند کالا", on_delete=models.CASCADE, null=True, related_name="brands")
+    is_active= models.BooleanField(default=True,blank=True, verbose_name="وضعیت فعال / غیر فعال")
     slug= models.SlugField(max_length=200, null=True)
+    register_date= models.DateTimeField(auto_now_add=True, verbose_name="تاریخ درج", )
+    published_date= models.DateTimeField(default=timezone.now, verbose_name="تاریخ انتشار")
+    update_date= models.DateTimeField(auto_now=True, verbose_name="تاریخ آخرین بروزرسانی")
     
-    def __str__(self):
+    def __str__(self) -> str:
         return self.product_name
     
     def get_absolute_url(self):
@@ -79,44 +81,51 @@ class Product(models.Model):
     
     
     class Meta:
-        verbose_name= 'کالا'
-        verbose_name_plural= 'کالا ها'
+        verbose_name= " کالا"
+        verbose_name_plural= "کالا ها"
     
-# #---------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------
 class FeatureValue(models.Model):
-    value_title = models.CharField(max_length=100,verbose_name='عنوان مقدار')
-    feature = models.ForeignKey(Feature,on_delete=models.CASCADE,blank=True,null=True,verbose_name='ویژگی',related_name='feature_values')
+    value_title= models.CharField(max_length=100, verbose_name="عنوان مقدار")
+    feature= models.ForeignKey(Feature, on_delete=models.CASCADE, blank=True, null=True, verbose_name="ویژگی", related_name="feature_value")
     
-    def __str__(self):
-        return f"{self.id} {self.value_title}"
+    def __str__(self) -> str:
+        return f"{self.value_title}"
     
     class Meta:
-        verbose_name = 'مقدار ویژگی'
-        verbose_name_plural = 'مقدار ویژگی ها'
-            
-
-#------------------------------------------------------------------------------------------------------------------------------
+        verbose_name= " مقدار ویژگی"
+        verbose_name_plural= "مقادیر ویژگی ها"
+    
+#-----------------------------------------------------------------------------------
 class ProductFeature(models.Model):
-    product= models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name= 'کالا', related_name='product_features')
-    feature= models.ForeignKey(Feature, on_delete=models.CASCADE, verbose_name= 'ویژگی')
-    value= models.CharField(max_length=100, verbose_name= 'مقدار ویژگی کالا')
-    filter_value = models.ForeignKey(FeatureValue,null=True,blank=True,on_delete=models.CASCADE,verbose_name='مقدار ویژگی برای فیلتر')        
+    product= models.ForeignKey(Product,on_delete=models.CASCADE, verbose_name="کالا", related_name='product_features')
+    feature= models.ForeignKey(Feature, on_delete=models.CASCADE, verbose_name="ویژگی")
+    value= models.CharField(max_length=100, verbose_name="مقدار ویژگی کالا")
+    
+    filter_value=  models.ForeignKey(FeatureValue, null=True, blank=True, on_delete= models.CASCADE, verbose_name="product_feature_value")
+    
     def __str__(self):
-        return f"{self.product} - {self.feature} : {self.value}"
+        return f"{self.product} - {self.feature} - {self.value}"
     
     class Meta:
-        verbose_name= 'ویژگی کالا'
-        verbose_name_plural= 'ویژگی های کالا'
+        verbose_name= "ویژگی کالا"
+        verbose_name_plural= "ویژگی ها ی کالا ها"
     
-# #---------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------
 class ProductGallery(models.Model):
-    product= models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name= 'کالا', related_name="gallery_images")
-    file_upload= FileUpload('image', 'product_gallery')
-    image_name= models.ImageField(upload_to=file_upload.upload_to, verbose_name='تصویر کالا')
+    product= models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="کالا", related_name='galery_images')
+    file_uplaod= FileUpload('images', 'product_gallery')
+    image_name= models.ImageField(upload_to=file_uplaod.upload_to, verbose_name="تصویر کالا")
+    
     
     class Meta:
-        verbose_name= 'تصویر'
-        verbose_name_plural= 'تصاویر'
+        verbose_name= "تصویر"
+        verbose_name_plural= "تصویر ها"
     
-# #---------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------
+    
+    
+    
+    
+    
     
--- shop_alma/apps/products/views.py
+++ shop_alma/apps/products/views.py
@@ -1,41 +1,46 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product, ProductGroup, Brand, FeatureValue
+from .models import Product, ProductGroup, FeatureValue, Brand
 from django.db.models import Q, Count, Min, Max
 from django.views import View
 from django.http import JsonResponse
-
+from .filters import ProductFilter
 from django.core.paginator import Paginator
-#---------------------------------------------------------------------------------------------------------------
-def root_group():
-    return ProductGroup.objects.filter(Q(is_active=True)& Q(group_parent=None))
 
-# -----------------------------ارزانترین محصولات که بر اساس قیمت مرتب شده اند----------------------------------
+
+def get_root_group():
+    return ProductGroup.objects.filter(Q(is_active=True) & Q(group_parent= None))
+
+#==========================ارزانترین محصولات=========================================
 def get_cheapest_products(request, *args, **kwargs):
-    products= Product.objects.filter(is_active=True).order_by('price')[:4]
-    product_groups=root_group()
-    context={
-        'products' : products,
-        'product_groups': product_groups,
+    products= Product.objects.filter(is_active= True).order_by('price')[:5]
+    product_groups= get_root_group()
+    context= {
+        "products": products,
+        "product_groups": product_groups
     }
     return render(request, "products_app/partials/cheapest_products.html", context)
 
-# -----------------------------جدیدترین محصولات به روز شده ----------------------------------
+
+#========================جدیدترین محصولات=======================================
 def get_last_products(request, *args, **kwargs):
-    products= Product.objects.filter(is_active=True).order_by('-published_date')[:4]
-    product_groups=root_group()
-    context={
-        'products' : products,
-        'product_groups': product_groups,
+    products= Product.objects.filter(is_active= True).order_by('-published_date')[:5]
+    product_groups= get_root_group()
+    context= {
+        "products": products,
+        "product_groups": product_groups
     }
     return render(request, "products_app/partials/last_products.html", context)
 
 #========================دسته های محبوب =======================================
-def get_popular_product_groups(request,*args,**kwargs):
-    product_groups=ProductGroup.objects.filter(Q(is_active=True)).annotate(count=Count('products_of_groups')).order_by('-count')[:6]
-    context={
-        'product_groups':product_groups
+def get_popular_product_group(request, *args, **kwargs):
+    # product_groups= ProductGroup.objects.filter(Q(is_active=True)).annotate(count= Count('product_of_groups')).order_by('-count')[:6]
+    product_groups= ProductGroup.objects.filter(Q(is_active=True))\
+                    .annotate(count= Count('product_of_groups'))\
+                    .order_by('-count')[:6]
+                    
+    context= {
+        "product_groups": product_groups
     }
-   
     return render(request, "products_app/partials/popular_product_group.html", context)
 
 #========================جزئیات محصول =======================================
@@ -45,113 +50,84 @@ class ProductDetailView(View):
         if product.is_active:
             return render(request, "products_app/product_detail.html", {'product':product})
 
-# ======================محصولات مرتبط =========================================================
-def get_related_products(request,*args,**kwargs):
-    current_product=get_object_or_404(Product,slug=kwargs['slug'])
-    related_products=[]
-    for group in current_product.product_group.all():
-        related_products.extend(Product.objects.filter(Q(is_active=True) & Q(product_group=group) & ~Q(id=current_product.id)))
-    return render(request,'products_app/partials/related_products.html',{'related_products':related_products})
+#======================== محصولات مرتبط=======================================
+def get_related_products(request, *args, **kwargs):
+    curent_products= get_object_or_404(Product, slug=kwargs['slug'])
+    related_products= []
+    for group in curent_products.product_group.all() :
+        related_products.extend(Product.objects.filter(Q(is_active=True) & (Q(product_group=group)) & ~Q(id= curent_products.id)))
 
-# لیست همه گروه های محصولات ========================================================================================================
-class ProductGroupView(View):
-    def get(self,request):
-        product_groups=ProductGroup.objects.filter(Q(is_active=True)).annotate(count=Count('products_of_groups'))\
-                                                                     .order_by('-count')
-        
-        return render(request,'products_app/product_groups.html/',{'product_groups':product_groups})
-# لیست گروه محصولات برای فیلتر--------------------------------------------------------------------------------------------------------------------------------------------
-def get_product_groups(request):
-    product_groups=ProductGroup.objects.annotate(count=Count('products_of_groups'))\
-                                                       .filter(Q(is_active=True) & ~Q(count=0))\
-                                                       .order_by('-count')
-    
-    return render(request, 'products_app/partials/product_groups.html' ,{'product_groups':product_groups})
+    return render(request, "products_app/partials/related_product.html", {'related_products':related_products})
 
-# لیست برندها برای فیلتر-# -----------------------------------------------------------------------------------------------------------------------------------------
-def get_brands(request,*args,**kwargs):
-    product_group = get_object_or_404(ProductGroup, slug=kwargs['slug'])
-    brand_list_id=product_group.products_of_groups.filter(is_active=True).values('brand_id')
-    brand=Brand.objects.filter(pk__in=brand_list_id)\
-        .annotate(count=Count('brands'))\
-        .filter(~Q(count=0))\
-        .order_by('-count')
-    
-    return render(request,'products_app/partials/brands.html',{'brand':brand, 'group_slug': kwargs['slug']})
-# #--لیست دیگر ویزگی ها بر حسب ویزگی های کالاهای درون گروه---------------------------------------------------------------------------------------------------------------
+#======================== همه دسته ها =======================================
+class ProductGroupView(View):   
+    def get(self, request):
+        product_groups= ProductGroup.objects.filter(Q(is_active=True))\
+                        .annotate(count= Count('product_of_groups'))\
+                        .order_by('-count')
+        return render(request, "products_app/product_groups.html/", {'product_groups':product_groups})
 
-def get_features_for_filter(request,*args,**kwargs):
-    product_group = get_object_or_404(ProductGroup, slug=kwargs['slug'])
-    feature_list=product_group.features_of_groups.all()
-    feature_dict=dict()
-    
-    for feature in feature_list :
-        feature_dict[feature]=feature.feature_values.all();
-    
-    return render(request,'products_app/partials/features_filter.html',{'feature_dict':feature_dict})
-        
-#-لیست محصولات هر گروه محصولات--------------------------------------------------------------------------------------------------------------
-class ProductByGroupView(View):
-    def get(self,request,*args,**kwargs):
-        slug = kwargs['slug']
-        current_group = get_object_or_404(ProductGroup,slug=slug)   #برو از پروداکت گروپ اسلاگ ها رو بیاور 
-        products = Product.objects.filter(Q(is_active=True) & Q(product_group=current_group))
-       
-        #اگر خواستی روی یه مجموعه ای یسری تابع اعمال کنی از aggregate استفاده میکنیم
-        res_aggre=products.aggregate(min=Min('price'),max=Max('price'))
-         
-        
+#======================== لیت محصولات دسته ها =======================================
+class ProductsByGroupView(View):
+    def get(self,request, *args, **kwargs):        
+        slug= kwargs['slug']
+        current_group= get_object_or_404(ProductGroup, slug=kwargs['slug'])
+        products= Product.objects.filter(Q(is_active=True) & Q(product_group=current_group))     
+        res_aggre= products.aggregate(min=Min('price'), max=Max('price'),)
+            
         #price filter
-        filter=ProductFilter(request.GET,queryset=products)
-        products=filter.qs 
-        
-        
-        #queryset=      لیستی که قراره فیلتر روی اون اعمال بشه
+        filter= ProductFilter(request.GET,queryset=products)
+        products= filter.qs
         
         #brand filter
-        brands_filter=request.GET.getlist('brand')
-        if brands_filter:
-            products=products.filter(brand__id__in=brands_filter )
-               
+        brand_filter= request.GET.getlist('brand')
+        if brand_filter:
+            products=products.filter(brand__id__in=brand_filter)
             
-        #feature filter
-        features_filter=request.GET.getlist('feature')
-        if features_filter:
-            products=products.filter(product_feature__filter_value__id__in=features_filter ).distinct()
+        #features filter
+        feature_filter= request.GET.getlist('feature')
+        if feature_filter:
+            products=products.filter(product_feature__filter_value__id__in= feature_filter).distinct()
             
-        #distinct       حذف تکراری ها 
-            
-    
-            
-        group_slug=slug 
-        product_per_page=10                         #تعداد کالاها در هر صفحه
-        paginator=Paginator(products, product_per_page)         
-        page_number=request.GET.get('page')                   #بدست اوردن شماره صفحه جاری
-        page_obj=paginator.get_page(page_number)              #لیست کالا ها بعد از صفحه بندی برای نمایش در صحفه جاری
-        product_count=products.count();                       #تعداد کل کالاهای موجود در این گروه
+        sort_type= request.GET.get('sort_type')
+        if not sort_type:
+            sort_type="0"
         
+        if sort_type=="1":
+            products= products.order_by('price')
         
-        #لیست اعداد برای ساخت منو باز شونده برای تعیین تعداد کالای هر صففحه توسط کاربر
-        show_count_product=[]
-        i=product_per_page
+        elif sort_type=="2":
+            products= products.order_by('-price')
+        
+        group_slug= slug
+        product_per_page=10                                     # تعداد کالاها در هر صفحه  
+        paginator= Paginator(products, product_per_page)        #
+        page_number= request.GET.get('page')                    # بدست آوردن شماره صفحه جاری
+        page_obj= paginator.get_page(page_number)               # لیست کالاها بعد از صفحه بندی برای نمایش در صفحه جاری
+        product_count= products.count()                         # تعداد کل کالاهای موجود در این گروه 
+        
+        # لیست اعداد برای ساخت منو باز شونده برای تعیین تعداد کالای هر صفحه توسط کاربر
+        show_count_product= []
+        i= product_per_page
         while i<product_count:
             show_count_product.append(i)
             i*=2
         show_count_product.append(i)
-
-
+        
         context={
             'products':products,
-            'current_group':current_group,
-            'res_aggre':res_aggre,
-            'group_slug':group_slug,
-            'page_obj':page_obj,
-            'product_count':product_count,
-            'show_count_product':show_count_product,
-             'filter':filter,
-            
+            'current_group': current_group,
+            'res_aggre': res_aggre,
+            'group_slug': group_slug,
+            'page_obj': page_obj,
+            'product_count': product_count,
+            'show_count_product': show_count_product,
+            'filter': filter,
+            'sort_type': sort_type,
         }    
-        return render(request,"products_app/products.html",{"products":products,'current_group':current_group})
+        
+        return render(request, "products_app/products.html/", context)
+
 
 #======================== جی کوئری ارتباط ویژگی ها =======================================
 def get_filter_value_for_feature(request):
@@ -162,4 +138,49 @@ def get_filter_value_for_feature(request):
 
         return JsonResponse(data=res, safe=False)
 
-        
+#========================= لیست گروه محصولات برای فیلتر =======================================
+def get_product_groups(request):
+    product_groups= ProductGroup.objects.annotate(count= Count('product_of_groups'))\
+                    .filter(Q(is_active= True) & ~Q(count=0))\
+                    .order_by('-count')
+    return render(request, 'products_app/partials/product_groups.html', {'product_groups': product_groups})
+
+#========================= لیست برند ها برای فیلتر =======================================
+def get_brands(request, *args, **kwargs):
+    product_group= get_object_or_404(ProductGroup, slug= kwargs['slug'])
+    brand_list_id= product_group.product_of_groups.filter(is_active=True).values('brand_id')
+    brands= Brand.objects.filter(pk__in= brand_list_id)\
+                        .annotate(count= Count('brands'))\
+                        .filter(~Q(count=0))\
+                        .order_by('-count')
+    return render(request, 'products_app/partials/brands.html', {'brands': brands})
+
+#========================= لیست های دیگر فیلترها بر حسب مقادیر ویژگیهای کالاهای درون گروه =======================================
+def get_feautres_for_filter(request, *args, **kwargs):
+    product_group= get_object_or_404(ProductGroup, slug= kwargs['slug'])
+    feature_list= product_group.feature_of_groups.all()
+    feature_dict= dict()
+    for feature in feature_list:
+        feature_dict[feature]= feature.feature_value.all()
+    
+    return render(request, 'products_app/partials/features_filter.html', {'feature_dict':feature_dict})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
